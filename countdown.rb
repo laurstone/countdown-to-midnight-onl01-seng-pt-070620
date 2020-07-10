163faf3e98e6a2cf6f3fd36c48a9c16d5260be29
Lauren Stone <lstone321@gmail.com>
2020-07-10 23:46:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+end
+if number == 0
+"HAPPY NEW YEAR!"
+end 
 
-def countdown
+def countdown_with_sleep(number)
+ runtime = Benchmark.measure (1.second)
+end
 end
