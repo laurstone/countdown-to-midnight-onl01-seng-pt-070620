388eb17afb52b9c0f9474f4420acca029e15e000
Lauren Stone <lstone321@gmail.com>
2020-07-11 00:53:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,3 +12,16 @@ end
 def countdown_with_sleep(number)
  sleep 5
 end
+
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+end
+  if number == 0
+  return "HAPPY NEW YEAR!"
+end
+
+
+end
